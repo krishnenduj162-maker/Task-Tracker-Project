1727ce3b10c6605eb25e53ed34994ae001388b7a
krishnendu <krishnenduj162@gmail.com>
2026-06-11 17:26:07 +0530

    Update backend URL

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
-
+const BASE_URL = "https://your-backend.up.railway.app";
 // GET all tasks
 export const getTasks = () => {
   return axios.get(`${BASE_URL}/tasks`);
